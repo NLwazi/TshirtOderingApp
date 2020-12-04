3eedf51f57635a8230f2324a1ff4f448615e55f3
Lwazi Nhleko <nlwazi@ymail.com>
2020-12-04 12:25:24 +0200

    First Commit

---
+++ TshirtApp/TshirtApp/TshirtApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+
+namespace TshirtApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void SaveButton_clicked(Object sender, System.EventArgs e)
+
+            Name name = new Name()
+            {
+                Name = nameEntry.Text,
+                Gender = genderEntry.Text,
+                TshirtSize = sizeEntry.Text,
+                DateOfOrder = dateEntry.Text,
+                TshirtColor = tshirtEntry.Text,
+                DateOfOrder = dateEntry.Text,
+                ShippingAddress = addressEntry.Text
+            };
+    }       using(SQLiteConnection conn= new SQLiteConnection(App.FilePath))
+            {
+        
+            }
+}
---
+++ TshirtApp/TshirtApp/TshirtApp/OrderDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Order;
+
+namespace TshirtApp
+{
+    class Order
+    {
+        public class Order
+        {
+            public string Name { get; set; }
+            public string Gender { get; set; }
+            public string DateOfOrder { get; set; }
+            public string TshirtSize { get; set; }
+
+            public string TshirtColor { get; set; }
+            public string ShippingAddress { get; set; }
+
+
+
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+    }
+}
